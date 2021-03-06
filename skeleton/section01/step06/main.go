2887924007b882eadf50905cfbd6dedcaa262649
task4233 <tks.m1205@gmail.com>
2021-03-07 00:38:00 +0900

    add step06

--- skeleton/section01/step06/main.go
+++ skeleton/section01/step06/main.go
@@ -20,4 +20,20 @@ func main() {
 	// TODO: 変数numが0〜79のときは"ノーマル"、
 	// 80〜94のときは"R"、95〜98のときは"SR"、
 	// それ以外のときは"XR"と表示する
+	var msg string
+	switch {
+	case 0 <= num && num < 80:
+		msg = "ノーマル"
+		break
+	case 80 <= num && num < 95:
+		msg = "R"
+		break
+	case 95 <= num && num < 98:
+		msg = "SR"
+		break
+	default:
+		msg = "XR"
+		break
+	}
+	fmt.Println(msg)
 }
