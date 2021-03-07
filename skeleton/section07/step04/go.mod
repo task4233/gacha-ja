147ac9e10b481aab4b4b5c175bb1cf5fc1f6905c
task4233 <tks.m1205@gmail.com>
2021-03-07 21:32:43 +0900

    add 07/04

--- skeleton/section07/step04/go.mod
+++ skeleton/section07/step04/go.mod
@@ -1,5 +1,5 @@
-module github.com/gohandson/gacha-ja
+module github.com/task4233/gacha-ja/skeleton/section07/step04
 
 go 1.15
 
-require github.com/tenntenn/sqlite v1.0.2
+require github.com/tenntenn/sqlite v1.0.2 // indirect
--- skeleton/section07/step04/main.go
+++ skeleton/section07/step04/main.go
@@ -10,7 +10,7 @@ import (
 	"os"
 	"strconv"
 
-	"github.com/gohandson/gacha-ja/gacha"
+	"github.com/task4233/gacha-ja/skeleton/section07/step04/gacha"
 	"github.com/tenntenn/sqlite"
 )
 
@@ -54,6 +54,7 @@ func run() error {
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		// TODO: データベースから結果を最大100件取得し、変数resultsに代入
+		results, err := getResults(db, 100)
 
 		if err != nil {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
@@ -113,7 +114,7 @@ func createTable(db *sql.DB) error {
 func saveResult(db *sql.DB, card *gacha.Card) error {
 	const sqlStr = `INSERT INTO results(rarity, name) VALUES (?,?);`
 	// TODO: Execメソッドを用いてINSERT文を実行する
-
+	_, err := db.Exec(sqlStr, card.Rarity, card.Name)
 	if err != nil {
 		return err
 	}
@@ -132,7 +133,7 @@ func getResults(db *sql.DB, limit int) ([]*gacha.Card, error) {
 	for rows.Next() {
 		var card gacha.Card
 		// TODO: rows.Scanメソッドを用いてレコードをcardのフィールドに読み込む
-
+		err := rows.Scan(&card.Rarity, &card.Name)
 		if err != nil {
 			return nil, fmt.Errorf("Scan:%w", err)
 		}
